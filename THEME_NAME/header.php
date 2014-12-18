b932fcac7f66e88a191984a6c7a77b8d37336f3b
Marco Biedermann <marco.biedermann@gmx.de>
2014-12-18 20:08:05 +0100

    add meta tags

--- THEME_NAME/header.php
+++ THEME_NAME/header.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
 
-  <meta charset="UTF-8">
-  <title>Document</title>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+  <link rel="shortcut icon" href="<?php echo IMAGES ?>/favicon.ico">
 
 </head>
 <body>
