1e1cc6ef2947f0bffe70cc38895c04b66fe5bd3b
Marco Biedermann <marco.biedermann@gmx.de>
2014-12-29 13:29:02 +0100

    error 404 page

---
+++ THEME_NAME/404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+  <main class="main">
+
+    <h1>Oops. Page not found!</h1>
+    <p>It seems that you are looking for something that is not here.</p>
+    <p>Return to <a href="<?php echo home_url(); ?>">homepage</a>.</p>
+
+  </main>
+
+</div>
+
+<?php get_footer(); ?>
