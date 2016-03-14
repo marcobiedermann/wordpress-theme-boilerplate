261644bd4ef49949e464f4fa5513e38b91b8650a
Marco Biedermann <marco.biedermann@gmx.de>
2016-03-14 20:42:49 +0100

    fix coding style

--- THEME_NAME/functions.php
+++ THEME_NAME/functions.php
@@ -1,42 +1,35 @@
 <?php
 
-  // Constants
+// Constants
+define( 'THEME_NAME', 'THEME_NAME' );
+define( 'THEMEROOT', get_stylesheet_directory_uri() );
+define( 'IMAGES', THEMEROOT . '/images' );
+define( 'SCRIPTS', THEMEROOT . '/js' );
 
-  define( 'THEME_NAME', 'THEME_NAME' );
-  define( 'THEMEROOT', get_stylesheet_directory_uri() );
-  define( 'IMAGES', THEMEROOT . '/images' );
-  define( 'SCRIPTS', THEMEROOT . '/js' );
+// Add Theme Support
+add_theme_support( 'title-tag' );
+add_theme_support( 'automatic-feed-links' );
 
-  // Add Theme Support
-  add_theme_support( 'title-tag' );
-  add_theme_support( 'automatic-feed-links' );
+// Register Menus
+register_nav_menus( array(
+  'primary' => __( 'Primary Menu', THEME_NAME )
+) );
 
-  // Register Menus
-  register_nav_menus( array(
-    'primary' => __( 'Primary Menu', THEME_NAME )
-  ) );
+// Register sidebar
+register_sidebar(
+  array(
+    'id'          => 'sidebar',
+    'description' => __( 'Sidebar content', THEME_NAME ),
+    'name'        => __( 'Sidebar', THEME_NAME ),
+  )
+);
 
-  // Register sidebar
-  register_sidebar(
-    array(
-      'id'          => 'sidebar',
-      'description' => __( 'Sidebar content', THEME_NAME ),
-      'name'        => __( 'Sidebar', THEME_NAME ),
-    )
-  );
+// Post format and featured Image
+add_theme_support( 'post-formats', array( 'link', 'quote', 'gallery' ) );
+add_theme_support( 'post-thumbnails', array( 'post' ) );
 
-  // Post format and featured Image
+// Load Styles
+wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1.0.0', 'all' );
 
-  if ( function_exists( 'add_theme_support' )) {
-    add_theme_support( 'post-formats', array( 'link', 'quote', 'gallery' ) );
-    add_theme_support( 'post-thumbnails', array( 'post' ) );
-  }
-
-  // Load JS
-
-  add_action( 'wp_enqueue_scripts', 'load_custom_scripts' );
-
-  function load_custom_scripts() {
-    wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1.0.0', 'all' );
-    wp_enqueue_script( 'main_script', THEMEROOT . '/js/main.js', array('jquery'), '1.0.0', true );
-  }
+// Load Scripts
+wp_enqueue_script( 'main_script', THEMEROOT . '/js/main.js', array('jquery'), '1.0.0', true );
