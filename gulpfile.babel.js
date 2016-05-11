b477b5347b8ec0ef762a386e413cc4ab0d294648
Marco Biedermann <marco.biedermann@gmx.de>
2016-05-11 22:16:54 +0200

    move from Scss to PostCSS

---
+++ gulpfile.babel.js
@@ -0,0 +1,34 @@
+import gulp           from 'gulp';
+import gulpPostcss    from 'gulp-postcss';
+import gulpSourcemaps from 'gulp-sourcemaps';
+import postcssCssnext from 'postcss-cssnext';
+import postcssImport  from 'postcss-import';
+
+const dirs = {
+  src: 'THEME_NAME',
+  dest: 'THEME_NAME'
+};
+
+gulp.task('css', () => {
+  gulp.src(`${dirs.src}/css/style.css`)
+    .pipe(gulpSourcemaps.init())
+    .pipe(gulpPostcss([
+      postcssImport(),
+      postcssCssnext({
+        features: {
+          rem: false
+        },
+      })
+    ]))
+    .pipe(gulpSourcemaps.write('.'))
+    .pipe(gulp.dest(`${dirs.dest}`));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(`${dirs.src}/css/**/*.css`, ['css']);
+});
+
+gulp.task('default', [
+  'css',
+  'watch'
+]);
