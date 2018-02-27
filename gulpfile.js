c6c383c2d4a504b8bc996f066e2f4028a73eabf1
Marco Biedermann <marco.biedermann@gmx.de>
2018-02-28 00:01:17 +0100

    replace spaces with tabs

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,32 @@ const postcssCssnext = require('postcss-cssnext');
 const postcssImport = require('postcss-import');
 
 const dirs = {
-  src: 'THEME_NAME',
-  dest: 'THEME_NAME',
+	src: 'THEME_NAME',
+	dest: 'THEME_NAME',
 };
 
 gulp.task('css', () => gulp.src(`${dirs.src}/css/style.css`)
-  .pipe(gulpSourcemaps.init())
-  .pipe(gulpPostcss([
-    postcssImport(),
-    postcssCssnext({
-      features: {
-        rem: false,
-      },
-    }),
-  ]))
-  .pipe(gulpSourcemaps.write('.'))
-  .pipe(gulp.dest(`${dirs.dest}`)));
+	.pipe(gulpSourcemaps.init())
+	.pipe(gulpPostcss([
+		postcssImport(),
+		postcssCssnext({
+			features: {
+				rem: false,
+			},
+		}),
+	]))
+	.pipe(gulpSourcemaps.write('.'))
+	.pipe(gulp.dest(`${dirs.dest}`)));
 
 gulp.task('watch', () => {
-  gulp.watch(`${dirs.src}/css/**/*.css`, ['css']);
+	gulp.watch(`${dirs.src}/css/**/*.css`, ['css']);
 });
 
 gulp.task('default', [
-  'css',
-  'watch',
+	'css',
+	'watch',
 ]);
 
 gulp.task('build', [
-  'css',
+	'css',
 ]);
